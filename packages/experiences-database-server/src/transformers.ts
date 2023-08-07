d00e3cb7366130fee805bac565ce9b12e0bdff14
Ota Prokopec <otaprokopec@gmail.com>
2023-08-07 20:59:26 +0200

    pkg

--- packages/experiences-database-server/src/transformers.ts
+++ packages/experiences-database-server/src/transformers.ts
@@ -1,11 +1,12 @@
 import type { Experience, ExperienceDocument } from '@app/ts-types'
-import { extend, omit } from 'lodash'
+import pkg from 'lodash'
+const { merge, omit } = pkg
 import { numberTimingCoords } from './settings'
 import { Models } from '@app/appwrite-server'
 
 export const transformExperienceDocumentIntoExperience = (...expDocuments: ExperienceDocument[]) => {
 	return expDocuments.map((exp) =>
-		extend(omit(exp, 'latitute', 'longitute'), {
+		merge(omit(exp, 'latitute', 'longitute'), {
 			location: [exp.latitute / numberTimingCoords, exp.longitute / numberTimingCoords],
 		}),
 	) as (Experience & Models.Document)[]
