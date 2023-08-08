739f3f334ae07ff4fed0bc68e7134a184f19a075
Ota Prokopec <otaprokopec@gmail.com>
2023-08-08 22:23:32 +0200

    load err

--- apps/experiences/src/lib/server/router.ts
+++ apps/experiences/src/lib/server/router.ts
@@ -1,9 +1,11 @@
 import { accountRouter } from './routers/account/router';
 import { experienceRouter } from './routers/experiences/router';
+import { profileRouter } from './routers/profile/router';
 import { router } from './trpc';
 export const appRouter = router({
 	account: accountRouter,
-	experience: experienceRouter
+	experience: experienceRouter,
+	profile: profileRouter
 });
 
 // Export only the type of a router!
