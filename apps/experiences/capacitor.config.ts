16604531ebbdfd6702826eb7f040c37007f70919
Ota Prokopec <otaprokopec@gmail.com>
2023-08-26 00:33:47 +0200

    cap config

---
+++ apps/experiences/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.example.app',
+	appName: 'experiences',
+	webDir: '.svelte-kit/output'
+};
+
+export default config;
