86796b4435af39425cbaf8004f2f78a6d534ad09
Ota Prokopec <otaprokopec@gmail.com>
2023-10-08 21:30:44 +0200

    delete unused packages

--- unusedPackages/appwrite-ssr/src/types/Types.ts
+++
@@ -1,6 +0,0 @@
-import { connections } from '../connections'
-
-export namespace Types {
-	export type Cookie = Record<'name' | 'value', string>
-	export type AppwriteSSR = ReturnType<typeof connections.setCookie>
-}
