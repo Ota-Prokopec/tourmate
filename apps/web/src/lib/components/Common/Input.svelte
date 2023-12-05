e0bc7accedc7bf0994b7210ba583091f3aef0ae0
Ota Prokopec <otaprokopec@gmail.com>
2023-12-05 18:54:39 +0100

    cookies strict

--- apps/web/src/lib/components/Common/Input.svelte
+++ apps/web/src/lib/components/Common/Input.svelte
@@ -118,12 +118,12 @@
 		use:retype
 		{id}
 		class={twMerge(
-			'rounded-3xl !m-0: p-4 text-gray-900 border border-gray-300 text-left outline-none appearance-none w-full',
+			'rounded-3xl !m-0: p-4 text-gray-900 border border-gray-300 text-left outline-none appearance-none w-full bg-gray-50',
 			icon && (iconPosition === 'left' ? 'pl-12' : 'pr-12'),
 			readOnly && 'cursor-pointer',
 			floatingLabel &&
-				'w-full text-gray-900 bg-transparent border-gray-300 dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
-			'bg-gray-50 text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400',
+				'w-full text-gray-900 border-gray-300 dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+			'text-gray-900 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400',
 			className
 		)}
 		{placeholder}
@@ -141,7 +141,7 @@
 		<label
 			for={id}
 			class={twMerge(
-				'absolute ml-4 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-700 rounded-full px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1',
+				'absolute ml-4 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-50 dark:bg-gray-700 rounded-full px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1',
 				iconPosition === 'left' ? 'pl-6' : iconPosition === 'right' ? 'pr-6' : ''
 			)}>{floatingLabel}</label
 		>
