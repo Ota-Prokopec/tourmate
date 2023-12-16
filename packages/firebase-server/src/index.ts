4a9ba64b49940595279a53726b81001a19bd6701
Ota Prokopec <otaprokopec@gmail.com>
2023-12-16 20:59:23 +0100

    fdsa

--- packages/firebase-server/src/index.ts
+++ packages/firebase-server/src/index.ts
@@ -8,11 +8,11 @@ const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/gm, '\n') ?? '
 if (!firebase.apps.length) {
 	const firebaseApp = initializeApp({
 		credential: admin.credential.cert({
-			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+			clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? 'notfound',
 			privateKey: privateKey,
-			projectId: process.env.FIREBASE_PROJECT_ID,
+			projectId: process.env.FIREBASE_PROJECT_ID ?? 'notfound',
 		}),
-		projectId: process.env.FIREBASE_PROJECT_ID,
+		projectId: process.env.FIREBASE_PROJECT_ID ?? 'notfound',
 	})
 }
 
