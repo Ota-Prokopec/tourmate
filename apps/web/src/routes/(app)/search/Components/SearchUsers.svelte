ccf3d9176875e46c7c114cd5f440028fdd7a5acc
Ota Prokopec <otaprokopec@gmail.com>
2024-01-08 20:02:33 +0100

    no content

--- apps/web/src/routes/(app)/search/Components/SearchUsers.svelte
+++ apps/web/src/routes/(app)/search/Components/SearchUsers.svelte
@@ -4,52 +4,40 @@
 	import UserItemSkeleton from '$lib/components/User/UserItemSkeleton.svelte';
 	import { sdk } from '$src/graphql/sdk';
 	import type { UserInfo } from '@app/ts-types';
+	import MonumentNotFoundDrawer from '../../createNewExperience/[lat]-[lng]/Components/MonumentNotFoundDrawer.svelte';
+	import Column from '$lib/components/Common/Column.svelte';
+	import NoContent from '$lib/components/Common/NoContent.svelte';
 
 	export let searchingText: string;
 	export let limit: number;
 
 	let users: UserInfo[] = [];
 
-	let initialLoading = false;
-	let loadingMoreItems = false;
+	let isLoading = false;
 
-	const loadUsers = async () => {
-		initialLoading = users.length === 0 ? true : false; // true only if there are no monuments in the usersMonuments
-		loadingMoreItems = users.length > 0 ? true : false; //true only when there already is some monument in the usersMonuments
+	const loadUsers = async (textValue: string) => {
+		isLoading = true; // true only if there are no monuments in the usersMonuments
 
 		await sdk
-			.getListOfUsers({ limit: limit, offset: users.length })
+			.getListOfUsers({ limit: limit, offset: users.length, searchingText: textValue })
 			.then(({ getListOfUsers: newUsers }) => {
-				users = [...users, ...newUsers];
+				users = newUsers;
 			});
-		initialLoading = false;
-		loadingMoreItems = false;
+		isLoading = false;
 	};
 
-	loadUsers();
+	loadUsers(searchingText);
+	$: loadUsers(searchingText);
 </script>
 
-<Paginating
-	wrapperClassName="w-full h-auto"
-	{loadingMoreItems}
-	let:item
-	{initialLoading}
-	list={users}
-	on:loadMore={loadUsers}
->
-	<svelte:fragment slot="loading">
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-		<UserItemSkeleton class="w-full" />
-	</svelte:fragment>
-
-	<UserItem class="w-full" user={item} />
-</Paginating>
+<Column class="gap-4 justify-center items-center w-full ">
+	{#if isLoading}
+		<UserItemSkeleton />
+	{:else if users && users?.length > 0}
+		{#each users as user}
+			<UserItem class="w-full" {user} />
+		{/each}
+	{:else}
+		<NoContent />
+	{/if}
+</Column>
