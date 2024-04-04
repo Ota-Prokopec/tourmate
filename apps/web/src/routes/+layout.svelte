db2fffb5d6a8cea5c10f2c9bfd1d595676ce2096
Ota Prokopec <otaprokopec@gmail.com>
2024-04-04 17:39:00 +0200

    vercel analytics

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -14,6 +14,7 @@
 	import { onMount } from 'svelte';
 	import '../app.css';
 	import { alertStore } from './alertStore';
+	import { inject } from '@vercel/analytics';
 
 	let systemStatus: TSystemHealthStatus | undefined = undefined;
 
@@ -39,6 +40,9 @@
 				graphqlService: false
 			};
 		});
+
+	//user's analytics
+	inject();
 </script>
 
 {#if systemStatus && (!systemStatus?.appwriteService || !systemStatus.graphqlService)}
