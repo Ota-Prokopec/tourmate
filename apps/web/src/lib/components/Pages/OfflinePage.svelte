589ce014bfa5427fb2b7f2252b0d49f14dfad51f
Ota Prokopec <otaprokopec@gmail.com>
2024-04-09 18:55:26 +0200

    offline

--- apps/web/src/lib/components/Pages/OfflinePage.svelte
+++ apps/web/src/lib/components/Pages/OfflinePage.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
-	import Alert from '$lib/components/Alert/Alert.svelte';
 	import Center from '$lib/components/Common/Center.svelte';
 	import Icon from '$lib/components/Common/Icon.svelte';
 	import Text from '$lib/components/Common/Text.svelte';
 	import IconNoWifi from '$lib/components/Icons/IconNoWifi.svelte';
-	import { P, Heading } from 'flowbite-svelte';
-	import { alert } from '../alertStore';
-
-	alert(
-		'No internet connection',
-		'Your internet connection is not working, please try to connect to internet and try Tourmate again.',
-		{ color: 'red' }
-	);
+	import { Heading } from 'flowbite-svelte';
 </script>
 
 <Center class="flex flex-wrap flex-col gap-16 justify-center items-center h-full w-full p-2">
-	<Heading tag="h1" class="font-bold w-auto h-auto">Offline</Heading>
+	<Heading tag="h1" class="font-bold w-auto h-auto">
+		<Text>Offline</Text>
+	</Heading>
 	<Icon class="child:w-16 child:h-16">
 		<IconNoWifi />
 	</Icon>
